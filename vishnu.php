1310a923140de7564c680b47d3281e1f1f76abff
kumarvishu6 <90055016+kumarvishu6@users.noreply.github.com>
2021-12-13 21:32:57 +0530

    Update vishnu.php

--- vishnu.php
+++ vishnu.php
@@ -1,14 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=thn, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=\, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+
+body {background-image: url(https://cdn.pixabay.com/photo/2021/02/16/18/55/gamer-6022003__340.png) ;
+	     
+  
+  background-repeat: no-repeat;
+  position: relative;
+  width: 85%;
+  height: 610px;
+  margin-left: auto;
+  margin-right: auto;
+  background-size: 100%;
+  }
+
+    </style>
 </head>
 <body>
+<br>
+<br>
+
+
+
     <?php 
-    if(isset($_POST['name'])){
+    //if(isset($_POST['name'])){
 
     
     $server = "localhost";
@@ -27,22 +47,22 @@
    $name = $_POST['name'];
    $email = $_POST['email'];
    $phone = $_POST['phone'];
-   $sql = "INSERT INTO `connectfour`.`game_data` (`name`, `email`, `phone`) VALUES ('$name', '$email', '$phone');";
+   $sql = "INSERT INTO `connectfour`.`minor` (`name`, `email`, `phone`) VALUES ('$name', '$email', '$phone');";
 
    //echo $sql;
 
   if($con->query($sql) == TRUE){
 
-    echo "successfully inserted";
+    echo "<p><center><h1>MUCH APPRECIATEED!</h1></center></p>";
+    echo "<h2><center>Your data is safe with us</center></h2>";
   } 
     else{
         echo "ERROR: $sql <br> $con->error";
     }
       $con->close();
-}
+//}
     
     ?>
     
     
-</body>
-</html>
+
